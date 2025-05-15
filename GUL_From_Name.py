03b0a40234a506f20d8ec1a966267ee9517b5f36
PavloDaniielian <pdaniielian@gmail.com>
2025-05-15 04:10:39 +0300

    added GetFromName

--- GUL_From_Name.py
+++ GUL_From_Name.py
@@ -54,7 +54,7 @@ client = MongoClient("mongodb://localhost:27017/")
 # Access a specific database
 db = client['MyDatas']  # Replace 'mydatabase' with your database name
 # Access a specific collection
-collectionList = db['GitHub_Users']  # Replace 'mycollection' with your collection name
+collectionList = db['GitHub_Users_DavidWoods']  # Replace 'mycollection' with your collection name
 
 dateJoinedLast = '2000-01-01T00:00:00Z'
 dateJoinedFrom = '2000-01-01T00:00:00Z'
@@ -72,21 +72,26 @@ while True:
         dateJoinedFrom = dateJoinedLast
 
     # Fetch users matching the search criteria
-    data = requestToGit("https://api.github.com/search/users", {'q': ("language:CSharp type:User created:>"+dateJoinedFrom), 'sort': 'joined', 'order': 'asc', 'page': page, 'per_page': 30})
+    data = requestToGit(
+        "https://api.github.com/search/users",
+        {
+            'q': 'type:User in:name "David" AND "Woods" created:>'+dateJoinedFrom,
+            'sort': 'joined', 'order': 'asc', 'page': page, 'per_page': 30
+        })
     print(f"total Count: {data['total_count']}, currentPage : {page}, catched_count : {origin_count+(page-1)*30}")
 
     # Store each user's login name for detailed profile fetch
-    user_logins = [user['login'] for user in data['items']]
+    user_logins = [user['login'] for user in data.get('items', [])]
     # Get detailed info for each user
     for login in user_logins:
         user_url = f"https://api.github.com/users/{login}"
-        user_data = requestToGit(user_url,params = {})
+        user_data = requestToGit(user_url, params = {})
 
-        # get repository
+        # Get repository
         user_url = f"https://api.github.com/users/{login}/repos"
         user_repos = requestToGit(user_url, params = {'page': 1, 'per_page': 1})
         email_adr = ''
-        if len(user_repos) > 0:
+        if user_repos and len(user_repos) > 0:
             repos_name = user_repos[0]['full_name']
             commit_data = requestToGit( f"https://api.github.com/repos/{repos_name}/commits", params = {'page': 1, 'per_page': 1})
             if commit_data and len(commit_data) > 0:
